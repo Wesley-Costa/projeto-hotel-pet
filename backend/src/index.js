3471fa0c67bdf25b51945a703ec0e5db87dacdd2
Wesley-Costa <wesleycosta06@hotmail.com>
2022-03-19 22:34:46 -0300

    alt config

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,23 +4,16 @@ const cors = require('cors');
 const app = express();
 const path = require('path')
 
-// app.use(express.json());
-// app.use('/files', express.static(path.resolve(__dirname,"temp", "uploads")));
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-//     res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
-//     next();
-// })
-// app.use(cors());
-// app.use(routes);
-
-app.use('/', 
-    express.static(
-        __dirname,
-        './build'
-    )
-)
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname,"temp", "uploads")));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
+    next();
+})
+app.use(cors());
+app.use(routes);
 
 app.listen(process.env.PORT || 3001, (err) => {
     if (err) {
